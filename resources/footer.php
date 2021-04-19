c24c50740040285afe8cbc3836bf946bd7398e67
ivan5420 <ivan5420@yandex.ru>
2021-04-19 14:52:34 +0300

    add privacy

--- resources/footer.php
+++ resources/footer.php
@@ -1,19 +1,49 @@
 
 <div class="footer-back">
+<?php include_once './resources/contact.php';
+    //  $site_url=$_SERVER['HTTP_HOST'];
+?>
 
-
-<div class="container footer">
+<div class="container footer white-text">
   <div class="row">
     <div class="col-xl-8 col-sm-12">
       <p>Богатырская Услада</p>
     </div>
-    <div class="col-xl-4 col-sm-12">
-
+    <div class="col-xl-4 col-sm-12 text-right">
+      <div class="footer-phone">
+        <a href="tel:<?=$contact['phone'];?>">  <?=$contact['phone'];?></a>
+      </div>
+      <div class="footer-contact">
+        <a href="mailto:<?=$contact['email'];?>">  <?=$contact['email'];?></a>
+      </div>
+      <div class="footer-contact">
+        <?=$contact['address'];?>
+      </div>
     </div>
+  </div>
+  <div class="row footer-data">
+    <div class="col-xl-8 col-sm-12 d-flex flex-wrap justify-content-between ip-data">
+      <span><?=$contact['ip_name'];?></span>
+      <span><?=$contact['ip_inn'];?></span>
+      <span><?=$contact['ip_ogrn'];?></span>
+      <span>Сайт не является публичной офертой и носит информационный характер.</span>
+    </div>
+    <div class="col-xl-4 col-sm-12 d-flex flex-wrap align-items-end justify-content-end">
+      <a href="/policy">Политика конфиденциальности</a>
+    </div>
+
   </div>
 
 </div>
 </div>
+
+
+
+
+
+
+
+
 
 
 <!-- Optional JavaScript -->
